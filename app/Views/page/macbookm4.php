92e2600cff45ba2225a07d85dd4b5a36e024f6dd
muthia <muthia@gmail.com>
2024-11-09 13:09:33 +0700

    muthia

---
+++ app/Views/page/macbookm4.php
@@ -0,0 +1,160 @@
+<?= $this->extend('page-layout/content-layout') ?>
+
+<?= $this->section('content') ?>
+<!-- content section -->
+<section id="content" class="content">
+    <div class="container mx-auto px-4 py-8">
+        <div class="flex flex-col lg:flex-row">
+            <div class="lg:w-2/3">
+                <div class="flex items-center mb-4">
+                    <span class="bg-red-600 text-white px-2 py-1 rounded mr-2">
+                        LAPTOP
+                    </span>
+                </div>
+                <h1 class="text-4xl font-bold mb-4">
+                    MacBook Pro M4 Hadir Membawa Webcam 12MP dan Daya Tahan Baterai 24 Jam
+                </h1>
+                <div class="flex items-center mb-4">
+                    <img alt="Author's profile picture" class="rounded-full w-12 h-12 mr-4" height="50" src="https://storage.googleapis.com/a1aa/image/Y9Rel7BNX3TZI6YZvYAvoq7qlVKbbNML3toqBZuIU7QHCtyJA.jpg" width="50" />
+                    <div>
+                        <p class="font-bold">
+                            Christopher Louis
+                        </p>
+                        <p class="text-gray-400">
+                            Published 31 October 2024 18:05
+                        </p>
+                    </div>
+                </div>
+                <img alt="Gaming setup with a monitor displaying a game, a gaming PC with RGB lighting, and gaming accessories" class="w-full mb-4" height="400" src="<?= base_url('css/img/MediaTek-Dimensity-9400.jpg') ?>" style="width: 900px;height:500px;border-radius:2%;object-fit:cover" />
+                <p class="text-gray-300 mb-4">
+                    <span class="font-bold">
+                        Jakarta, GadgetGlimpse
+                    </span>
+                    – Malam kemarin (30/10) merupakan malam terakhir dari rangkaian peluncuran produk Mac terbaru dari Apple, yang ditayangkan satu per satu baik melalui rilis hingga video pengenalan dari kanal YouTube resmi Apple. Menjadi yang ketiga dan terakhir, giliran MacBook Pro M4 Series yang akhirnya debut. Memperkenalkan opsi prosesor terbaru yang jauh lebih kencang, serta peningkatan aspek sana-sini.
+                </p>
+                <p class="text-gray-300 mb-4">Sejak generasi M3, Apple sudah tidak lagi hadirkan MacBook Pro dalam dimensi layar 13 inci. Sebagai gantinya, MacBook Pro M4 kelas entri membawa sejumlah keunggulan yang sebelumnya hanya tersedia pada versi cip M-Pro atau M-Max. Dalam generasi terbaru kali ini, akhirnya konsumen bisa mendapatkan MacBook Pro termurah, dalam opsi warna Space Black.
+                </p>
+                <p class="text-gray-300 mb-4">Ada hal menarik yang juga turut diinformasikan oleh Apple dalam pengenalan MacBook Pro M4, di mana Apple secara resmi mengumumkan semua perangkat Mac terbaru, kini hadir dalam RAM terendah mulai 16GB. Tidak hanya jajaran Mac M4, MacBook Air M2 & M3 yang masih ada di pasaran, juga bakal kena imbas positif satu ini, tanpa adanya kenaikan harga.</p>
+
+
+                <p class="text-white-300 font-bold mb-3" style="font-size: 20px;">Baterai Lebih Besar, Layar MacBook Pro M4 Juga Lebih Cerah MacBook Pro M4 </p>
+                <img alt="Gaming setup with a monitor displaying a game, a gaming PC with RGB lighting, and gaming accessories" class="w-full mb-4 mx-auto" height="400" src="<?= base_url('css/img/2200_1.png') ?>" style="width: 900px;height:500px;border-radius:2%;object-fit:cover;" />
+                <p class="text-white-300 mb-4" style="text-align: justify;">Sebelum membahas performa, apa saja yang baru dari MacBook Pro M4, selain pada aspek yang utamanya dicari oleh konsumen kantoran atau kreator profesional? Secara tampilan desain sendiri masih sama, di mana hadir dalam opsi 14 maupun 16 inci. Namun ada sejumlah peningkatan termasuk pada segi layar, kamera, dan baterai.</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Ketika diintip lewat situs resmi Apple, layar MacBook Pro M4 tetap membawa tingkat kecerahan maksimum hingga 1600 nits untuk konten HDR melalui panel mini-LED 120Hz yang dimilikinya. Namun untuk konten SDR, kini bisa sampai 1000 nits dalam kondisi outdoor, jauh meningkat dari 600 nits pada generasi sebelumnya.</p>
+                <img alt="Gaming setup with a monitor displaying a game, a gaming PC with RGB lighting, and gaming accessories" class="w-full mb-4 mx-auto" height="400" src="<?= base_url('css/img/2200_1.png') ?>" style="width: 900px;height:500px;border-radius:2%;object-fit:cover;" />
+                <p class="text-white-300 mb-4" style="text-align: justify;">Kemudian pada bagian notch yang lebar di atas layar, Apple akhirnya membawa kecanggihan kamera selfie iPad, ke MacBook Pro M4, alias sebuah sensor 12MP Center Stage yang punya sudut pandang sangat lebar. Fungsinya? Secara otomatis mampu mengatur framing sesuai jumlah orang, bahkan mendukung Desk View agar bisa menunjukkan apa yang ada di depan layar, dari tampilan kamera seolah berada di atas.</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Jumlah port USB-C juga meningkat pada opsi entri MacBook Pro M4, ada tiga, dan semuanya mendukung standar Thunderbolt 4, melengkapi port lain seperti jack audio, HDMI, kartu SD, sampai MagSafe. Baterai MacBook Pro M4 kini juga diklaim bisa sampai 24 jam, menjadikannya MacBook dengan daya tahan baterai paling awet. Selain berkat cip M4, juga kapasitas baterainya yang dibuat lebih besar (72,4WHr pada MacBook Pro M4 standar).</p>
+
+
+                <p class="text-white-300 font-bold mb-3" style="font-size: 20px;">Varian dengan Cip M4 Pro & M4 Max Untuk Performa Jauh Lebih Kencang</p>
+                <img alt="Gaming setup with a monitor displaying a game, a gaming PC with RGB lighting, and gaming accessories" class="w-full mb-4 mx-auto" height="400" src="<?= base_url('css/img/2200_1.png') ?>" style="width: 900px;height:500px;border-radius:2%;object-fit:cover;" />
+                <p class="text-white-300 mb-4" style="text-align: justify;">Hadir untuk konsumen yang mengutamakan performa, Apple ingin memikat mereka yang masih menggunakan produk Mac dengan cip M1, atau yang masih pakai varian dengan prosesor Intel. Dibandingkan cip M1, peningkatan performa CPU bisa hampir dua kali lipat, dengan Neural Engine tiga kali lebih kencang.</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Untuk mereka yang inginkan performa benar-benar kencang, tersedia pilihan MacBook Pro M4 Pro dengan hingga 14 inti CPU dan 20 inti GPU, atau Macbook Pro M4 Max yang membawa hingga 16 inti CPU serta 40 inti GPU. Apple mengatakan bila opsi tertingginya cocok untuk memproses efek visual dan animasi 3D kompleks, sampai large language models (LLMs) berkat dukungan RAM sampai 128GB.</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Yang menjadi pembeda lainnya pada versi cip M4 Pro dan M4 Max, adalah penyematan standar Thunderbolt 5 di masing-masing port USB-C. Tentunya, sudah menjalankan macOS Sequoia, yang telah dibekali Apple Intelligence. Dengan fitur yang bakal terus ditambahkan seiring dengan berjalannya waktu.</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Harga MacBook Pro M4 versi entri dengan RAM 16GB dan penyimpanan internal 512GB, dibanderol mulai USD1599 atau sekitar Rp25 jutaan. Apple juga memberikan opsi nano-texture display selayaknya iPad Pro dan iMac M4 terbaru, dengan biaya tambahan. Laptop Apple terbaru ini mulai dijual di toko secara luring pada 8 November mendatang.</p>
+
+            </div>
+            <div class="lg:w-1/3 lg:pl-8">
+                <div class="sticky top-4 mb-8">
+                    <h2 class="text-xl font-bold mb-4 border-b border-pink-600 pb-2">
+                        Latest Stories
+                    </h2>
+                    <div class="flex items-center mb-4">
+                        <img alt="Thumbnail of the article" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/8tyi3ikLqir3FtUn3arArt3JMxAo9OTAGt5aXqhOGzHAE46E.jpg" width="100" />
+                        <div>
+                            <p class="font-bold">
+                                Tips Rakit PC Gaming Edisi 2024, Mudah dan Simpel!
+                            </p>
+                            <p class="text-white-400">
+                                Komputer / Tips
+                            </p>
+                        </div>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <img alt="Thumbnail of the article" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/PjDvyYxpmkYdBBmaTNF1plEzbVYNk61dXuq2QFFLSOeePgrTA.jpg" width="100" />
+                        <div>
+                            <p class="font-bold">
+                                Laporan Ungkap Gen Z Sering Gunakan Pay Later
+                            </p>
+                            <p class="text-white-400">
+                                Business
+                            </p>
+                        </div>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <img alt="Thumbnail of the article" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/i0zrHVkCVf0seU6BecIt9B0ZJinljmxNlFcDZV1fGEHzfBcdC.jpg" width="100" />
+                        <div>
+                            <p class="font-bold">
+                                Virtus Showcase 2024 Bahas Tantangan Transformasi Bisnis Menggunakan AI
+                            </p>
+                            <p class="text-white-400">
+                                News
+                            </p>
+                        </div>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <img alt="Thumbnail of the article" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/fLwtdMygcjXDSSGMdBjwO44hYNieYXnKm3J8zYtmIty6PgrTA.jpg" width="100" />
+                        <div>
+                            <p class="font-bold">
+                                IFS Beberkan Manfaat Teknologi AI Bagi Perusahaan di Indonesia
+                            </p>
+                            <p class="text-white-400">
+                                Teknologi
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <h2 class="text-xl font-bold mb-4 border-b border-pink-600 pb-2">
+                            Latest Review
+                        </h2>
+                        <div class="flex items-center mb-4">
+                            <img alt="Thumbnail of the review" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/57JpYjF3TK6RIFqafr44NJ4Ox7AfxwSkUp2GJyzyLL1UEalTA.jpg" width="100" />
+                            <div>
+                                <p class="font-bold">
+                                    Review ZTE Blade A35: Nggak Sampai Sejuta, Bawa Gimmick Menarik
+                                </p>
+                                <p class="text-white-400">
+                                    Review
+                                </p>
+                            </div>
+                        </div>
+                        <div class="flex items-center mb-4">
+                            <img alt="Thumbnail of the review" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/57JpYjF3TK6RIFqafr44NJ4Ox7AfxwSkUp2GJyzyLL1UEalTA.jpg" width="100" />
+                            <div>
+                                <p class="font-bold">
+                                    Review vivo X100 Pro: Hadir Belakangan, Kualitas Foto Jempolan
+                                </p>
+                                <p class="text-white-400">
+                                    Review
+                                </p>
+                            </div>
+                        </div>
+                        <div class="flex items-center mb-4">
+                            <img alt="Thumbnail of the review" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/57JpYjF3TK6RIFqafr44NJ4Ox7AfxwSkUp2GJyzyLL1UEalTA.jpg" width="100" />
+                            <div>
+                                <p class="font-bold">
+                                    Review vivo X100 Pro: Hadir Belakangan, Kualitas Foto Jempolan
+                                </p>
+                                <p class="text-white-400">
+                                    Review
+                                </p>
+                            </div>
+                        </div>
+                        <div class="flex items-center mb-4">
+                            <img alt="Thumbnail of the review" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/57JpYjF3TK6RIFqafr44NJ4Ox7AfxwSkUp2GJyzyLL1UEalTA.jpg" width="100" />
+                            <div>
+                                <p class="font-bold">
+                                    Review vivo X100 Pro: Hadir Belakangan, Kualitas Foto Jempolan
+                                </p>
+                                <p class="text-white-400">
+                                    Review
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?= $this->endSection() ?>
