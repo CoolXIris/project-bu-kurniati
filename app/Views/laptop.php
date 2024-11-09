933b5b998b8c7de61379bb41d5bdeac9489a1f95
CoolXIris <lightningblue452@gmail.com>
2024-11-09 18:45:15 +0700

    save to server

--- app/Views/laptop.php
+++ app/Views/laptop.php
@@ -25,7 +25,7 @@
             <img src="<?= base_url('css/img/MediaTek-Dimensity-9400.jpg') ?>" alt="News Image">
             <span class="badge-bottom-left">MOUSE GAMING</span>
         </div>
-        <a href="<?= base_url('page/mediatek_dimensity9400') ?>" class="head_line">Razer Luncurkan Viper V3 Pro Sentinels Edition: Kolaborasi Ikonik dengan Tim Esports Sentinels</a>
+        <a href="<?= base_url('page/razerv3') ?>" class="head_line">Razer Luncurkan Viper V3 Pro Sentinels Edition: Kolaborasi Ikonik dengan Tim Esports Sentinels</a>
         <p class="description">Razer bersama dengan Sentinels merilis mouse gaming lewat Viper V3 Pro Sentinels…</p>
         <p class="author"><span style="font-style: bold;">Prasetyo Herfianto</span> | 10 October 2024</p>
     </div>
@@ -34,7 +34,7 @@
             <img src="<?= base_url('css/img/XGS-PON-WiFi-7.jpeg') ?>" alt="News Image">
             <span class="badge-bottom-left">AKSESORIS KOMPUTER</span>
         </div>
-        <a href="#" class="head_line">Corsair Luncurkan Keyboard K70 PRO TKL, Punya Teknologi MGX Hyperdrive untuk Gamer Kompetitif</a>
+        <a href="<?= base_url('page/k70pro') ?>" class="head_line">Corsair Luncurkan Keyboard K70 PRO TKL, Punya Teknologi MGX Hyperdrive untuk Gamer Kompetitif</a>
         <p class="description">Inovasi terbaru Corsair untuk para gamer tercermin lewat fitur unggulan keyboard K70…</p>
         <p class="author">
             <bold>Christopher Louis</bold> | 10 October 2024
@@ -46,7 +46,7 @@
             <img src="<?= base_url('css/img/mobile-banking-ilustrasi.jpg') ?>" alt="News Image">
             <span class="badge-bottom-left">AYANEO 3</span>
         </div>
-        <a href="#" class="head_line">Teaser Ayaneo 3 Muncul, Siap Jadi Ancaman Baru di Segmen PC Handheld</a>
+        <a href="<?= base_url('page/ayaneo3') ?>" class="head_line">Teaser Ayaneo 3 Muncul, Siap Jadi Ancaman Baru di Segmen PC Handheld</a>
         <p class="description">Begini penampakan awal pada Ayaneo 3, digadang sebagai PC Gaming Handheld dengan…</p>
         <p class="author">Christopher Louis | 09 October 2024</p>
 
@@ -56,7 +56,7 @@
             <img src="<?= base_url('css/img/Tiktok.jpeg') ?>" alt="News Image">
             <span class="badge-bottom-left">AMD REYZEN</span>
         </div>
-        <a href="#" class="head_line">AMD Luncurkan Prosesor Ryzen 7 9800X3D: Inovasi Terbaru untuk Pengalaman Gaming Maksimal</a>
+        <a href="<?= base_url('page/ryzen9800') ?>" class="head_line">AMD Luncurkan Prosesor Ryzen 7 9800X3D: Inovasi Terbaru untuk Pengalaman Gaming Maksimal</a>
         <p class="description">AMD Ryzen 7 9800X3D Siap Meluncur, Dirancang khusus para gamer yang mengincar…</p>
         <p class="author">Christopher Louis | 10 November 2024</p>
 
@@ -66,7 +66,7 @@
             <img src="<?= base_url('css/img/indosat-HiFi.jpeg') ?>" alt="News Image">
             <span class="badge-bottom-left">ASUS</span>
         </div>
-        <a href="#" class="head_line">ASUS Zenbook S 14 Diresmikan, Laptop AI Tipis dan Ringan Terbaik Seharga Rp28 Juta</a>
+        <a href="<?= base_url('page/zenbook14') ?>" class="head_line">ASUS Zenbook S 14 Diresmikan, Laptop AI Tipis dan Ringan Terbaik Seharga Rp28 Juta</a>
         <p class="description">Dengan harga premium, ASUS Zenbook S 14 terbaru bawa sejumlah keunggulan menarik.</p>
         <p class="author">Christopher Louis | 15 November 2024</p>
 
@@ -76,7 +76,7 @@
             <img src="<?= base_url('css/img/Samsung-Galaxy-A16.jpeg') ?>" alt="News Image">
             <span class="badge-bottom-left">ASUS</span>
         </div>
-        <a href="#" class="head_line">Motherboard ASUS Z890 Series, Berikan Solusi Baru Bagi Gamer Hingga Kreator</a>
+        <a href="<?= base_url('page/asusz890') ?>" class="head_line">Motherboard ASUS Z890 Series, Berikan Solusi Baru Bagi Gamer Hingga Kreator</a>
         <p class="description">Asus resmi merilis motherboard terbarunya, ASUS Z890 Series yang mencakup ekosistem Prime,…</p>
         <p class="author">Christopher Louis | 8 October 2024</p>
 
@@ -86,7 +86,7 @@
             <img src="<?= base_url('css/img/Synology-Solution.jpg') ?>" alt="News Image">
             <span class="badge-bottom-left">APPLE</span>
         </div>
-        <a href="#" class="head_line">MacBook Pro M4 Hadir Membawa Webcam 12MP dan Daya Tahan Baterai 24 Jam</a>
+        <a href="<?= base_url('page/macbookm4') ?>" class="head_line">MacBook Pro M4 Hadir Membawa Webcam 12MP dan Daya Tahan Baterai 24 Jam</a>
         <p class="description">MacBook Pro M4 kembali tersedia dalam tiga opsi cip berbeda dan dua…</p>
         <p class="author">Christopher Louis | 12 Desember 2024</p>
 
@@ -96,7 +96,7 @@
             <img src="<?= base_url('css/img/Asus-NUC.jpeg') ?>" alt="News Image">
             <span class="badge-bottom-left">APPLE</span>
         </div>
-        <a href="#" class="head_line">Mac Mini M4 Punya Ukuran Jauh Lebih Kecil, Performa Lebih Kencang</a>
+        <a href="<?= base_url('page/macmini4') ?>" class="head_line">Mac Mini M4 Punya Ukuran Jauh Lebih Kecil, Performa Lebih Kencang</a>
         <p class="description">auh lebih mungil, Mac Mini M4 tak sampai setengah ukuran generasi sebelumnya.</p>
         <p class="author">Prasetyo Herfianto | 20 November 2024</p>
 
@@ -106,7 +106,7 @@
             <img src="<?= base_url('css/img/Synology-Solution.jpg') ?>" alt="News Image">
             <span class="badge-bottom-left">#TIPS & TRIK</span>
         </div>
-        <a href="#" class="head_line">Inilah 3 Cara Efektif Membersihkan Laptop dari Debu & Minyak</a>
+        <a href="<?= base_url('page/3cra_efektif') ?>" class="head_line">Inilah 3 Cara Efektif Membersihkan Laptop dari Debu & Minyak</a>
         <p class="description">Cara membersihkan laptop dari debu dan minyak ternyata mudah, dan berikut adalah…</p>
         <p class="author">Christopher Louis | 16 Oktober 2024</p>
 
