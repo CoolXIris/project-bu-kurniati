58af49a365bc7851ca36589c2c4e427370e8dd88
muthia <muthia@gmail.com>
2024-11-13 11:33:38 +0700

    muthia

---
+++ app/Views/page/geminilive.php
@@ -0,0 +1,189 @@
+<?= $this->extend('page-layout/content-layout') ?>
+
+<?= $this->section('content') ?>
+<!-- content section -->
+<section id="content" class="content">
+    <div class="container mx-auto px-4 py-8">
+        <div class="flex flex-col lg:flex-row">
+            <div class="lg:w-2/3">
+                <div class="flex items-center mb-4">
+                    <span class="bg-red-600 text-white px-2 py-1 rounded">
+                        TIPS
+                    </span>
+                </div>
+                <h1 class="text-4xl font-bold mb-4">
+                    Gemini Live Bisa Bahasa Indonesia, Begini Cara Aktifkannya
+                </h1>
+                <div class="flex items-center mb-4">
+                    <img alt="Author's profile picture" class="rounded-full w-12 h-12 mr-4" height="50" src="https://storage.googleapis.com/a1aa/image/Y9Rel7BNX3TZI6YZvYAvoq7qlVKbbNML3toqBZuIU7QHCtyJA.jpg" width="50" />
+                    <div>
+                        <p class="font-bold">
+                            Bambang Dwi Atmoko
+                        </p>
+                        <p class="text-gray-400">
+                            Published 10 Agustus 2024 18:05
+                        </p>
+                    </div>
+                </div>
+                <img alt="Gaming setup with a monitor displaying a game, a gaming PC with RGB lighting, and gaming accessories" class="w-full mb-4" height="400" src="<?= base_url('css/img/MediaTek-Dimensity-9400.jpg') ?>" style="width: 900px;height:500px;border-radius:2%;object-fit:cover" />
+                <p class="text-gray-300 mb-4">
+                    <span class="font-bold">
+                        Jakarta, GadgetGlimpse
+                    </span>
+                    – Google belum lama ini mengumumkan bahwa mereka menghadirkan Gemini Live yang sudah bisa berbahasa Indonesia. Google terus mengembangkan Gemini sebagai asisten AI unggulan mereka, sehingga para pengguna di Indonesia dengan mudah dan nyaman bisa mengoperasikan Gemini Live. Mengingat, pada awalnya Gemini hanya bisa digunakan menggunakan bahasa Inggris, dan jika digunakan menggunakan Bahasa Indonesia hasilnya tidak begitu maksimal.
+                </p>
+                <p class="text-gray-300 mb-4">Maka dari itu, Google pada akhirnya menghadirkan update terbaru mereka dalam dunia Artificial Intelligence lewat Gemini yang sudah berbahasa Indonesia. Sebelumnya, Gemini Live mengumumkan telah mendukung lebih dari 40 bahasa dan belum termasuk Indonesia. Jadi, ini merupakan update yang cukup memberikan kegirangan kepada pengguna di Indonesia, banyak pengguna yang juga sepakat bahwa Gemini Live merupakan salah satu asisten AI yang cukup canggih.
+                </p>
+                <p class="text-gray-300 mb-4">Gemini Live dapat mendukung percakapan hingga dua bahasa sekaligus, memberikan fleksibilitas untuk beralih dari Bahasa Indonesia dan bahasa lainnya yang sudah dikuasai sebelumnya. Maka dari itu, ini merupakan sebuah fitur yang memungkinkan untuk berinteraksi dengan AI sesuai dengan konteks percakapan. Hanya saja masih banyak orang yang bingung untuk mengaktifkan Gemini Live berbahasa Indonesia.</p>
+
+
+                <p class="text-white-300 font-bold mb-3" style="font-size: 20px;">Cara Mengaktifkan Bahasa Indonesia di Gemini Live</p>
+                <img alt="Gaming setup with a monitor displaying a game, a gaming PC with RGB lighting, and gaming accessories" class="w-full mb-4 mx-auto" height="400" src="<?= base_url('css/img/2200_1.png') ?>" style="width: 900px;height:500px;border-radius:2%;object-fit:cover;" />
+                <p class="text-white-300 mb-4" style="text-align: justify;">Jangan khawatir, karena kami akan memberikan langkah mudah yang bisa kamu lakukan agar mampu dalam mengaktifkan Bahasa Indonesia pada platform Gemini Live. Berikut adalah langkah yang bisa kamu ikuti:</p>
+
+                <p class="text-white-300 mb-4" style="text-align: justify;">1 . Pada smartphone atau tablet, kamu bisa buka Google dan pada bagian atas klik gambar profil kamu seperti biasa.</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">2 . Masuk ke Setelan dan Asisten Google, kemudian piliha Bahasa.</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">3 . Pilih Bahasa Indonesia.</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">4 . Kamu bisa memilih satu bahasa lagi yang sudah didukung.</p>
+
+                <p class="text-white-300 mb-4" style="text-align: justify;">Dengan kamu mengikut langkah-langkah tersebut, kamu sudah bisa mengaktifkan Gemini Live menggunakan Bahasa Indonesia. Pastinya cara ini merupakan cara yang sudah direkomendasikan oleh Google, sehingga sudah teramin tentang kemudahannya dan keamanannya.</p>
+
+
+                <p class="text-white-300 font-bold mb-3" style="font-size: 20px;">Tentang Google Gemini</p>
+                <img alt="Gaming setup with a monitor displaying a game, a gaming PC with RGB lighting, and gaming accessories" class="w-full mb-4 mx-auto" height="400" src="<?= base_url('css/img/2200_1.png') ?>" style="width: 900px;height:500px;border-radius:2%;object-fit:cover;" />
+                <p class="text-white-300 mb-4" style="text-align: justify;">Google Gemini adalah proyek kecerdasan buatan dari Google yang menandai kemajuan dalam bidang model bahasa besar (Large Language Model/LLM). Gemini dirancang sebagai generasi penerus dari Google Bard, menggabungkan kekuatan AI dengan model bahasa yang lebih maju untuk pemahaman dan pemrosesan data yang lebih baik.</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Salah satu keunggulan Gemini adalah kemampuannya dalam pemrosesan gambar, teks, dan data kompleks secara lebih efisien. Gemini tidak hanya memahami konteks dengan lebih baik, tetapi juga menggabungkan informasi visual dan tekstual secara bersamaan untuk memberikan hasil yang lebih akurat.</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Google mengembangkan Gemini dengan fokus pada peningkatan interaksi manusia dengan AI, memungkinkan aplikasi di berbagai sektor seperti bisnis, pendidikan, dan kesehatan. Selain itu, Gemini diposisikan untuk menyaingi model AI dari perusahaan besar lainnya, seperti GPT dari OpenAI.</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Dengan teknologi canggih ini, Google berharap Gemini akan membantu pengguna memecahkan masalah yang lebih kompleks, memperluas kapabilitas teknologi AI, dan mempercepat pengembangan produk-produk berbasis kecerdasan buatan dalam waktu dekat.</p>
+
+
+
+                <p class="text-white-300 font-bold mb-3" style="font-size: 20px;">Gunakan Kuas Kecil untuk Debu di Sela-sela Keyboard</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Sela-sela keyboard adalah tempat di mana debu sering kali tersangkut. Untuk membersihkannya, kamu bisa menggunakan kuas kecil yang lembut. Arahkan kuas di sela-sela tombol untuk mengangkat debu dan kotoran kecil yang terperangkap. Jika memiliki peniup debu atau compressed air, alat ini bisa digunakan untuk meniup debu dari sela-sela keyboard dengan lebih efektif. Hindari meniup langsung dengan mulut agar tidak ada uap air yang menempel pada keyboard.</p>
+
+                <p class="text-white-300 font-bold mb-3" style="font-size: 20px;">Bersihkan Permukaan Keyboard dan Touchpad dengan Kain Basah</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Untuk membersihkan minyak atau noda pada permukaan keyboard dan touchpad, gunakan kain microfiber yang dibasahi sedikit dengan air atau cairan pembersih ringan. Usapkan kain perlahan-lahan ke seluruh permukaan keyboard dan touchpad. Hindari penggunaan kain yang terlalu basah agar tidak ada air yang meresap ke dalam komponen laptop.</p>
+
+                <p class="text-white-300 font-bold mb-3" style="font-size: 20px;">Gunakan Cotton Bud untuk Area Sulit Dijangkau</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Untuk membersihkan sudut-sudut kecil atau bagian keyboard yang sulit dijangkau, kamu bisa menggunakan cotton bud yang sedikit dibasahi. Cotton bud ini sangat efektif untuk membersihkan sisa-sisa debu atau minyak yang menempel di area yang sempit. Gunakan cotton bud dengan lembut agar tidak merusak tombol atau komponen di sekitar keyboard.</p>
+
+
+                <p class="text-white-300 font-bold mb-3" style="font-size: 20px;">3. Tips Tambahan untuk Menjaga Kebersihan Laptop</p>
+                <img alt="Gaming setup with a monitor displaying a game, a gaming PC with RGB lighting, and gaming accessories" class="w-full mb-4 mx-auto" height="400" src="<?= base_url('css/img/2200_1.png') ?>" style="width: 900px;height:500px;border-radius:2%;object-fit:cover;" />
+                <p class="text-white-300 mb-4" style="text-align: justify;">Selain membersihkan bagian layar dan keyboard, ada beberapa tips tambahan yang bisa kamu terapkan untuk menjaga laptop tetap bersih dan bebas dari debu dan minyak dalam jangka waktu yang lebih lama:</p>
+
+                <p class="text-white-300 font-bold mb-3" style="font-size: 20px;">Hindari Makan dan Minum di Dekat Laptop</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Salah satu penyebab laptop cepat kotor adalah kebiasaan makan atau minum di dekat laptop. Sisa makanan dan percikan minuman bisa membuat laptop mudah kotor atau bahkan merusak komponen di dalamnya. Jika memungkinkan, jauhkan laptop saat sedang makan atau minum untuk mengurangi risiko noda dan kerusakan.</p>
+
+                <p class="text-white-300 font-bold mb-3" style="font-size: 20px;">Gunakan Pelindung Keyboard</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Jika kamu sering mengetik atau menggunakan laptop dalam waktu lama, pertimbangkan untuk menggunakan pelindung keyboard. Selain menjaga keyboard dari debu dan minyak, pelindung ini juga dapat melindungi laptop dari kemungkinan kerusakan akibat tumpahan cairan. Pelindung keyboard bisa dilepas dan dibersihkan dengan mudah, sehingga mempermudah perawatan laptop.</p>
+
+                <p class="text-white-300 font-bold mb-3" style="font-size: 20px;">Lakukan Pembersihan Secara Rutin</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Debu dan minyak bisa menumpuk jika laptop tidak dibersihkan secara berkala. Lakukan pembersihan ringan setiap satu atau dua minggu sekali, terutama pada bagian layar dan keyboard. Dengan membersihkan secara rutin, laptop Anda akan selalu terlihat bersih, nyaman digunakan, dan performanya pun tetap optimal.</p>
+                <p class="text-white-300 mb-4" style="text-align: justify;">Menjaga kebersihan laptop adalah bagian penting dalam merawat perangkat agar tetap awet dan berfungsi dengan baik. Dengan menerapkan langkah-langkah pembersihan ini secara rutin, Anda tidak hanya akan menjaga tampilan laptop agar selalu terlihat rapi, tetapi juga membantu memperpanjang umur laptop dan memastikan performanya tetap prima.</p>
+
+            </div>
+            <div class="lg:w-1/3 lg:pl-8">
+                <div class="sticky top-4 mb-8">
+                    <h2 class="text-xl font-bold mb-4 border-b border-pink-600 pb-2">
+                        Latest Stories
+                    </h2>
+                    <div class="flex items-center mb-4">
+                        <img alt="Thumbnail of the article" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/8tyi3ikLqir3FtUn3arArt3JMxAo9OTAGt5aXqhOGzHAE46E.jpg" width="100" />
+                        <div>
+                            <p class="font-bold">
+                                Tips Rakit PC Gaming Edisi 2024, Mudah dan Simpel!
+                            </p>
+                            <p class="text-white-400">
+                                Komputer / Tips
+                            </p>
+                        </div>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <img alt="Thumbnail of the article" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/PjDvyYxpmkYdBBmaTNF1plEzbVYNk61dXuq2QFFLSOeePgrTA.jpg" width="100" />
+                        <div>
+                            <p class="font-bold">
+                                Laporan Ungkap Gen Z Sering Gunakan Pay Later
+                            </p>
+                            <p class="text-white-400">
+                                Business
+                            </p>
+                        </div>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <img alt="Thumbnail of the article" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/i0zrHVkCVf0seU6BecIt9B0ZJinljmxNlFcDZV1fGEHzfBcdC.jpg" width="100" />
+                        <div>
+                            <p class="font-bold">
+                                Virtus Showcase 2024 Bahas Tantangan Transformasi Bisnis Menggunakan AI
+                            </p>
+                            <p class="text-white-400">
+                                News
+                            </p>
+                        </div>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <img alt="Thumbnail of the article" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/fLwtdMygcjXDSSGMdBjwO44hYNieYXnKm3J8zYtmIty6PgrTA.jpg" width="100" />
+                        <div>
+                            <p class="font-bold">
+                                IFS Beberkan Manfaat Teknologi AI Bagi Perusahaan di Indonesia
+                            </p>
+                            <p class="text-white-400">
+                                Teknologi
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <h2 class="text-xl font-bold mb-4 border-b border-pink-600 pb-2">
+                            Latest Review
+                        </h2>
+                        <div class="flex items-center mb-4">
+                            <img alt="Thumbnail of the review" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/57JpYjF3TK6RIFqafr44NJ4Ox7AfxwSkUp2GJyzyLL1UEalTA.jpg" width="100" />
+                            <div>
+                                <p class="font-bold">
+                                    Review ZTE Blade A35: Nggak Sampai Sejuta, Bawa Gimmick Menarik
+                                </p>
+                                <p class="text-white-400">
+                                    Review
+                                </p>
+                            </div>
+                        </div>
+                        <div class="flex items-center mb-4">
+                            <img alt="Thumbnail of the review" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/57JpYjF3TK6RIFqafr44NJ4Ox7AfxwSkUp2GJyzyLL1UEalTA.jpg" width="100" />
+                            <div>
+                                <p class="font-bold">
+                                    Review vivo X100 Pro: Hadir Belakangan, Kualitas Foto Jempolan
+                                </p>
+                                <p class="text-white-400">
+                                    Review
+                                </p>
+                            </div>
+                        </div>
+                        <div class="flex items-center mb-4">
+                            <img alt="Thumbnail of the review" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/57JpYjF3TK6RIFqafr44NJ4Ox7AfxwSkUp2GJyzyLL1UEalTA.jpg" width="100" />
+                            <div>
+                                <p class="font-bold">
+                                    Review vivo X100 Pro: Hadir Belakangan, Kualitas Foto Jempolan
+                                </p>
+                                <p class="text-white-400">
+                                    Review
+                                </p>
+                            </div>
+                        </div>
+                        <div class="flex items-center mb-4">
+                            <img alt="Thumbnail of the review" class="w-24 h-16 mr-4" height="60" src="https://storage.googleapis.com/a1aa/image/57JpYjF3TK6RIFqafr44NJ4Ox7AfxwSkUp2GJyzyLL1UEalTA.jpg" width="100" />
+                            <div>
+                                <p class="font-bold">
+                                    Review vivo X100 Pro: Hadir Belakangan, Kualitas Foto Jempolan
+                                </p>
+                                <p class="text-white-400">
+                                    Review
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?= $this->endSection() ?>
